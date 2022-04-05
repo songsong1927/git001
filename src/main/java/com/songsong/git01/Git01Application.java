66001f4802f5b552330dee113c90a070b619e1aa
songsong1927 <1271015985@qq.con>
2022-04-05 19:23:54 +0800

    第一次提交

---
+++ src/main/java/com/songsong/git01/Git01Application.java
@@ -0,0 +1,13 @@
+package com.songsong.git01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Git01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Git01Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/songsong/git01/Git01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.songsong.git01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Git01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
